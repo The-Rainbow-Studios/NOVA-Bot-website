696e9fbdd53aaac2583b2e9d912b2c5ba173ba33
Tejas Lamba <tejas22feb@gmail.com>
2023-06-07 12:42:10 +0530

    feat: Add Navbar and Offcanvas to the App component
    This commit adds a Navbar and Offcanvas menu to the top of the App component, which lets users navigate quickly and provides additional, collapsible content.

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,49 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+//now take Navbar and compose a navbar with links
+<div>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  <Offcanvas
+    show={false}
+    onHide={() => {}}
+    placement="end"
+  >
+    <Offcanvas.Header closeButton>
+      <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+    </Offcanvas.Header>
+    <Offcanvas.Body>
+      Some text as placeholder. In real life you can have the elements you
+      have chosen. Like, text, images, lists, etc.
+    </Offcanvas.Body>
+  </Offcanvas>
+
       <h1>Hello People</h1>
       <p className="read-the-docs">
         This site is under construction, Check back later!
       </p>
-    </>
+    </div>
   )
 }
